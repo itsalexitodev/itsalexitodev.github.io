8557362438deb2a046eff26937a3d348892dc34f
its.alexito_dev <alexgro2015@gmail.com>
2023-10-27 08:54:30 +0200

    conectando pagina con google analytics

---
+++ recursos/google_analytics.js
@@ -0,0 +1,10 @@
+< script async src = "https://www.googletagmanager.com/gtag/js?id=G-M6HC5XRVS4" > < /script> <
+    script >
+    window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    window.dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-M6HC5XRVS4'); <
+/script>
